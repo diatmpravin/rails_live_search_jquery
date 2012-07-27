c0b0bc6797b5f625351f71b83d8c46627818139a
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-07-27 14:27:59 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLiveSearchJquery::Application.config.secret_token = '4e8e4906793b4188eb519c834e0a13f76bb06649bbe4fe7f1ed0a29ffbb38c91ea7abd97d7f5ed3bb51df8cfa6df75029339c0de65f2816729c21ebe49185c9d'
